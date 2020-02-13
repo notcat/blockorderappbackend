2471202b2a7681eecda30c981ef0ba9c62a99f42
damon <41898000+notcatt@users.noreply.github.com>
2020-02-13 13:15:09 -0800

    added express server, added routes for the api, /api/blockorderexample and /api/blockorder

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
 
+var express = require("express");
+var app = express();
+
+const port = 16942;
+
+var blockOrderJSON;
+
 axios.get('https://gfss.sd51.bc.ca').then((response) => {
         // Load the web page source code into a cheerio instance
     const $ = cheerio.load(response.data)
@@ -21,7 +28,7 @@ axios.get('https://gfss.sd51.bc.ca').then((response) => {
         // We proceed, only if the element exists
         if (blockP && i == 0) { // 0 1 2 3
             const blockText = $(blockP).text()
-
+            blockOrderJSON = blockText;
             console.log("-------- \n"+blockText+"\n-------- \n")
         }
     }
@@ -29,8 +36,30 @@ axios.get('https://gfss.sd51.bc.ca').then((response) => {
 
 // ([a-zA-Z]+)\s–\s(.),\s(.),\s(.) .+\s.,\s(.)
 
-// regex to parse (Wendsday) (A) (B) (C) (D)
+// regex to parse (Wednesday) (A) (B) (C) (D)
 
 
 // TODO:
 // regex REMOVE from the string using the regex above ^ to get any extra information, i.e. "FAMILY DAY, NO SCHOOL MONDAY."
+
+
+app.listen(port, () => {
+    console.log("Server running on port "+port);
+});
+
+app.get("/api/blockorderexample", (req, res, next) => {
+    res.json({
+        "container1": {
+            "Monday": ["A","B","C","D"],
+            "Tuesday": ["B","C","D","A"],
+            "Wednesday": ["C","D","A","B"],
+            "Thursday": ["D","A","B","C"],
+        }
+    });
+});
+
+app.get("/api/blockorder", (req, res, next) => {
+    res.json(blockOrderJSON);
+});
+
+// container(i) > Day(monday) > blockorder in indivudual elements, (a)(b)(c)(d) > the result of regex - string, i.e. extra text like MONDAY NO SCHOOL
