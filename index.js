69ca22c82d88479231d9208e4d667d7639e2a0c4
damon <41898000+notcatt@users.noreply.github.com>
2020-02-13 09:57:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
 
-const $ = cheerio.load('<p id="example">This is an <strong>example</strong> paragraph</p>')
-
-
 axios.get('https://gfss.sd51.bc.ca').then((response) => {
         // Load the web page source code into a cheerio instance
     const $ = cheerio.load(response.data)
 
-    // The pre.highlight.shell CSS selector matches all `pre` elements
-    // that have both the `highlight` and `shell` class
+    // gets a array of each container, and there can be 3+ containers
+    // usually there is 1-2 block order containers with 1 extra container for school
+    // information such as registration forms but there is the off chance
+    // where there is 4 containers.
     const blockContainer = $('div.et_pb_blurb_container')
 
     // We now loop through all the elements found
     for (let i = 0; i < blockContainer.length; i++) {
-        // Since the URL is within the span element, we can use the find method
-        // To get all span elements with the `s1` class that are contained inside the
-        // pre element. We select the first such element we find (since we have seen that the first span
-        // element contains the URL)
+        // there are 3+ containers on the website
+        // each has its own paragraph of information
+        // Monday - A B C (Lunch) C D
         const blockP = $(blockContainer[i]).find('div.et_pb_blurb_description')[0]
 
         // We proceed, only if the element exists
         if (blockP && i == 0) { // 0 1 2 3
             const blockText = $(blockP).text()
 
-            // We then print the text on to the console
             console.log("-------- \n"+blockText+"\n-------- \n")
         }
     }
@@ -32,11 +29,8 @@ axios.get('https://gfss.sd51.bc.ca').then((response) => {
 
 // ([a-zA-Z]+)\s–\s(.),\s(.),\s(.) .+\s.,\s(.)
 
-// regex to parse (Wensday) (A) (B) (C) (D)
-
-//console.log(dom.window.document.getElementsByClassName("et_pb_blurb_container").firstChild);
+// regex to parse (Wendsday) (A) (B) (C) (D)
 
 
-
-
-//"https://gfss.sd51.bc.ca",
+// TODO:
+// regex REMOVE from the string using the regex above ^ to get any extra information, i.e. "FAMILY DAY, NO SCHOOL MONDAY."
